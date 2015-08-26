8e3638658412d9a5039cf25b4dc179eaade62fa0
Ba55ie <svankerkfort@gmail.com>
2015-08-26 14:17:44 +0200

    Remove Intl polyfill
    It is an anti-pattern to include polyfills. The runtime should be
    patched.  See
    http://facebook.github.io/react/docs/working-with-the-browser.html#browser-support-and-polyfills

--- src/vi18n.js
+++ src/vi18n.js
@@ -5,12 +5,21 @@ define(function() {
 	var instances = {};
 
 	function VI18N(locale, currency) {
+		if (!VI18N.isSupported()) {
+			throw new Error('window.Intl not supported, did you forget to include polyfill?');
+		}
+
 		locale = locale || 'nl-NL';
 		currency = currency || 'EUR';
 
 		this.locale = locale;
 		this.currency = currency;
 		this.formatters = {};
+
+		// Keep track of this instance
+		instances[locale] = this;
+
+		this.initialize();
 	}
 
 	VI18N.prototype = {
@@ -24,8 +33,6 @@ define(function() {
 			this.formatters.number = new window.Intl.NumberFormat(locale);
 			this.formatters.currency = new window.Intl.NumberFormat(locale, { style: 'currency', currency: currency, minimumFractionDigits: 2, maximumFractionDigits: 2 });
 			this.formatters.date = new window.Intl.DateTimeFormat(locale);
-
-			return this;
 		},
 
 		getLocale: function() {
@@ -63,52 +70,6 @@ define(function() {
 		formatTime: function(date) {
 			return this.formatDate(date, { hour: '2-digit', minute: '2-digit',	second: '2-digit' });
 		}
-
-	};
-
-	// Static methods
-	VI18N.create = function create(options) {
-		var locale = options.locale,
-			currency = options.currency,
-			callback = options.callback,
-			instance;
-
-		if (typeof callback !== 'function') {
-			throw new Error('Callback should be defined');
-		}
-
-		instance = new VI18N(locale, currency);
-
-		instances[instance.getLocale()] = instance;
-
-		VI18N.polyfill(instance, callback);
-	};
-
-	VI18N.polyfill = function polyfill(instance, callback) {
-
-		if (VI18N.isSupported()) {
-			return callback(null, instance.initialize());
-		}
-
-		// When not natively suppported, load the polyfill with locale data and resolve
-		require([
-			'intl',
-			'text!../bower_components/intl/locale-data/json/' + instance.getLocale() + '.json'
-		], function(Intl, json) {
-
-			if (!json) {
-				return callback(new Error('Could not load locale JSON data'));
-			}
-
-			// Parse and add the data
-			Intl.__addLocaleData(JSON.parse(json));
-
-			callback(null, instance.initialize());
-
-		}, function(error) {
-
-			callback(error);
-		});
 
 	};
 
