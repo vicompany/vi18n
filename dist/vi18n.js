00600cfcbf161385cc4e4c89647af88d8d5d31f2
Ba55ie <Ba55ie@users.noreply.github.com>
2015-08-27 09:01:51 +0200

    Rename instances object to locales

--- dist/vi18n.js
+++ dist/vi18n.js
@@ -16,11 +16,12 @@
 
 	'use strict';
 
-	var instances = {};
+	var locales = {};
 
 	function VI18N(locale, currency) {
+		// Fail fast in case the Intl isn't supported
 		if (!VI18N.isSupported()) {
-			throw new Error('window.Intl not supported, did you forget to include polyfill?');
+			throw new Error('window.Intl not supported, did you forget to include a polyfill?');
 		}
 
 		locale = locale || 'nl-NL';
@@ -31,7 +32,7 @@
 		this.formatters = {};
 
 		// Keep track of this instance
-		instances[locale] = this;
+		locales[locale] = this;
 
 		this.initialize();
 	}
@@ -75,7 +76,7 @@
 			return this.formatters.currency.format(number);
 		},
 
-		// Format a data object to a locale string
+		// Format a date object to a locale string
 		// For more information see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat
 		formatDate: function(date, options) {
 			return typeof options === 'object' ? new this.formatters.date.constructor(this.getLocale(), options).format(date) : this.formatters.date.format(date);
@@ -88,7 +89,7 @@
 	};
 
 	VI18N.get = function get(locale) {
-		return instances[locale];
+		return locales[locale];
 	};
 
 	VI18N.isSupported = function isSupported() {
--- src/vi18n.js
+++ src/vi18n.js
@@ -2,11 +2,12 @@ define(function() {
 
 	'use strict';
 
-	var instances = {};
+	var locales = {};
 
 	function VI18N(locale, currency) {
+		// Fail fast in case the Intl isn't supported
 		if (!VI18N.isSupported()) {
-			throw new Error('window.Intl not supported, did you forget to include polyfill?');
+			throw new Error('window.Intl not supported, did you forget to include a polyfill?');
 		}
 
 		locale = locale || 'nl-NL';
@@ -17,7 +18,7 @@ define(function() {
 		this.formatters = {};
 
 		// Keep track of this instance
-		instances[locale] = this;
+		locales[locale] = this;
 
 		this.initialize();
 	}
@@ -61,7 +62,7 @@ define(function() {
 			return this.formatters.currency.format(number);
 		},
 
-		// Format a data object to a locale string
+		// Format a date object to a locale string
 		// For more information see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat
 		formatDate: function(date, options) {
 			return typeof options === 'object' ? new this.formatters.date.constructor(this.getLocale(), options).format(date) : this.formatters.date.format(date);
@@ -74,7 +75,7 @@ define(function() {
 	};
 
 	VI18N.get = function get(locale) {
-		return instances[locale];
+		return locales[locale];
 	};
 
 	VI18N.isSupported = function isSupported() {
